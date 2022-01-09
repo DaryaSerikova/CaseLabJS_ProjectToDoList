7e8e00f41fdd8593be71205935b29d8d568d9a07
DaryaSerikova <dvserikova@gmail.com>
2022-01-10 03:21:15 +0700

    delete all completed cases

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const todoList = document.getElementById('todo-list');
+let todoList = document.getElementById('todo-list');
 const userSelect = document.getElementById('user-todo');
 const form = document.querySelector('form');
+const buttonDeleteAllTodo = document.getElementById('deleteAllTodo');
 
 
 let todos = [];
 let users = [];
+// let idForSubmit = 201;
+// let idFlag = 0;
 
 document.addEventListener('DOMContentLoaded', initApp);
 form.addEventListener('submit', handlerSubmit);
+buttonDeleteAllTodo.addEventListener('click', hundleDeleteAll);
 
 function getUserName(userId){
     const user = users.find(u => u.id === userId);
@@ -18,6 +22,7 @@ function printTodo({userId, id, title, completed}) {
     const li = document.createElement('li');
     li.className = 'todo-item';
     li.dataset.id = id;
+    
     if (completed) {
         li.innerHTML = `<span class="liText strikethroughText"> ${title} <i>by</i> <b>${getUserName(userId)}<b> </span>`;
     } else {
@@ -66,7 +71,11 @@ function removeTodo(todoId) {
     todo.querySelector('input').removeEventListener('change', handlerCheckboxChange);
     todo.querySelector('.close').removeEventListener('click', handleClose);
 
+    console.log(`${todoId} элемент удален`);
     todo.remove();
+    //ВАЖНОЕ ЗАМЕЧАНИЕ! placeholder всегда при создании возвращает новый id=11 при POST, 
+    //поэтому все новые элементы списка имеют одинаковый id => удаление происходит некорректно,
+    //т.к. удалеяется только один 11ый элемент, а остальные остаются в списке
 }
 
 function alertError(error) {
@@ -75,24 +84,30 @@ function alertError(error) {
 
 
 
+
 function handlerSubmit(event) {
     event.preventDefault(); //иначе форма попробует отправиться синхронно с перезагрузкой страницы, приложение работать не будет
+    console.log(`form.todo.value = ${form.todo.value} title`); //обращаемся по name
+    console.log(`form.user.value = ${form.user.value} userId`); // тут тоже
+
+    console.log(idForSubmit);
     createTodo({
         userId : Number(form.user.value),
+        // id : idForSubmit,////////////////////////////////////////////////////////////////////////////////
         title : form.todo.value,
         completed : false
     });
-
+    // idForSubmit++;//////////////////////////////////////////////////////////////////////////////////
 }
 
 function handlerCheckboxChange() {
     const todoId = this.parentElement.dataset.id;
     const completed = this.checked;
-    console.log(completed);
+    console.log(`${todoId} элемент изменил статус, завершен:${completed}`);
 
     toggleTodoCompeted(todoId, completed);
 
-    if(completed) {////////////////////////////////////////////////////////////////////
+    if(completed) {
         this.nextElementSibling.classList.add('strikethroughText');
     } else {
         this.nextElementSibling.classList.remove('strikethroughText');
@@ -101,14 +116,26 @@ function handlerCheckboxChange() {
 
 function handleClose() {
     const todoId = this.parentElement.dataset.id;
-    // console.log(todoId);
+
     deleteTodo(todoId);
 }
+
+function hundleDeleteAll() {
+    let elements = document.querySelectorAll('.strikethroughText');
+
+    elements.forEach(elem => {
+        const todoId = elem.parentElement.dataset.id;
+
+        deleteTodo(todoId);
+    })
+
+}
+
 
 
 async function getAllTodos() {
     try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=25');
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=50');
         const data = await response.json();
     
         return data;
@@ -161,7 +188,7 @@ async function toggleTodoCompeted(todoId, completed) {
         });
     
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
         if (!response.ok) {
             alert('Sorry, error with connecting...');
         }
@@ -180,14 +207,11 @@ async function deleteTodo(todoId) {
             },
         });
         const data = await response.json();
-        //console.log(data);
+        // console.log(data);
         if(response.ok) {
             removeTodo(todoId);
-        } else {
-            alert('Sorry, error with connecting...');
         }
     } catch(error) {
         alertError(error);
     }
-
 }
